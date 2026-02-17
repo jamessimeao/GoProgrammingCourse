f8203d1d77916e89ffd174204bf51339c540a853
james <jamessimeao@outlook.com>
2026-02-17 07:37:49 -0300

    Math

---
+++ 7-math/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Abs(-5))
+	fmt.Println(math.Pi)
+	fmt.Println(math.E)
+	fmt.Println(math.Cos(math.Pi))
+	fmt.Println(math.Log(math.E))
+	fmt.Println(math.Sqrt(2))
+	fmt.Println(math.Pow(2, 3))
+}
